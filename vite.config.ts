e691284c85f179b7698be2a69e17962f3f927643
Natnat999 <natnat999play@gmail.com>
2026-04-16 17:37:48 +0200

    Force relative paths and add .nojekyll for GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/0x0_file_sharing/',
+  base: './', // Use relative paths for better compatibility
 })
